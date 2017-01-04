d0c573f3c0b80f0d8817c92f80b9250d95d1cf28
Nikola Majkić <n1kolaa924@gmail.com>
2017-01-04 09:34:57 +0000

    d801: rebrand
    Change-Id: I51564cca53a0d7c0be5381855f8958e65b1f1567

--- lineage.mk
+++ lineage.mk
@@ -1,4 +1,4 @@
-# Inherit some common CM stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
@@ -6,7 +6,7 @@ $(call inherit-product, device/lge/d801/d801.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := d801
-PRODUCT_NAME := cm_d801
+PRODUCT_NAME := lineage_d801
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-D801
 PRODUCT_MANUFACTURER := lge
